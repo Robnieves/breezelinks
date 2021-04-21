3ad56fe663f6e4fa445548830382cc58504c5ad2
Robnieves <rob.nieves@gmail.com>
2021-04-21 07:20:19 -0400

    Add files via upload
    Updated links so they open in new windows

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 function getEmail (){
 
   let email = document.querySelector("#emailInput").value;
-  document.querySelector("#title").innerHTML = "<i>Copy These Links</i>";
-
-  let adviceLink = "https://www.breezechms.com/open/unsubscribe?email=" + email + "&blog=updates";
-  let updatesLink = "https://www.breezechms.com/open/unsubscribe?email=" + email + "&blog=updates";
-
-  document.querySelector("#text1").innerHTML = adviceLink;
-  document.querySelector("#text2").innerHTML = updatesLink;
+  document.querySelector("#title").innerHTML = "Click the Following Links";
 
 
+let adviceAddress = "https://www.breezechms.com/open/unsubscribe?email=" + email + "&blog=updates";
+let updatesAddress = "https://www.breezechms.com/open/unsubscribe?email=" + email + "&blog=updates";
+
+console.log (adviceAddress);
+console.log (updatesAddress);
+
+let newTab = 'target="_blank"';
+
+let adviceLink = "<a href=" + adviceAddress + " " + newTab + " >Unsubscribe The Advice Blog</a>";
+let updatesLink = "<a href=" + updatesAddress + " " + newTab + ">Unsubscribe The Updates Blog</a>";
+
+console.log (adviceLink);
+console.log (updatesLink);
+
+document.querySelector("#text1").innerHTML = adviceLink;
+document.querySelector("#text2").innerHTML = updatesLink;
 
 }
